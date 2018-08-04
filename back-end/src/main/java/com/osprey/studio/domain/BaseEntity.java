ad52c4922da9c681e7af4ebce847acba3a85bc10
Vasil Babich <bv.dolphin.ua@gmail.com>
2018-08-04 18:34:49 +0300

    Correct base entity and file ignore git.

--- back-end/src/main/java/com/osprey/studio/domain/BaseEntity.java
+++ back-end/src/main/java/com/osprey/studio/domain/BaseEntity.java
@@ -16,6 +16,8 @@ import javax.persistence.MappedSuperclass;
 @ToString
 @MappedSuperclass  //  авто авторайд в другие классы
 public abstract class BaseEntity {
+    public static final int LENGTH_ENUM = 20;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
