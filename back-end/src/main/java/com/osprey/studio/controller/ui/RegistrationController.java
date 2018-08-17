1752bc2fd2423eddb9de4afb03f240a998f2fe66
moreyl <yan.alferov@yahoo.com>
2018-08-17 20:08:54 +0300

    last version

--- back-end/src/main/java/com/osprey/studio/controller/ui/RegistrationController.java
+++ back-end/src/main/java/com/osprey/studio/controller/ui/RegistrationController.java
@@ -1,13 +1,22 @@
 package com.osprey.studio.controller.ui;
 
+import com.osprey.studio.domain.entities.User;
 import com.osprey.studio.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ControllerUtils;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 
@@ -22,7 +31,6 @@ public class RegistrationController {
     public String getSignUpPage() {
         return "registration";
     }
-
 
 
 
